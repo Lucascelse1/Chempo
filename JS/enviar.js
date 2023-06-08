348f371d668c23464e1d92ef1748bcf0336b3097
Lucas Celse <lucas.j.celse@gmail.com>
2023-06-08 20:58:41 -0300

    Ultimos cambios 08/06/2023

--- JS/enviar.js
+++ JS/enviar.js
@@ -18,3 +18,29 @@ document.getElementById('form').addEventListener('submit', function(event) {
       alert(JSON.stringify(err));
     });
 });
+
+// Obtener los checkboxes por su nombre y asignar función al evento change
+const checkboxes = document.getElementsByName('check_id');
+const contactMethod = []; // Array para almacenar los métodos de contacto seleccionados
+
+checkboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('change', function() {
+    // Verificar si el checkbox está seleccionado
+    if (this.checked) {
+      // Obtener el texto del label asociado al checkbox
+      const label = this.nextElementSibling;
+      const labelText = label.value;
+
+      // Almacenar el método de contacto en el array
+      contactMethod.push(labelText);
+    } else {
+      // Si el checkbox se deselecciona, remover el método de contacto del array
+      const label = this.nextElementSibling;
+      const labelText = label.value;
+      const index = contactMethod.indexOf(labelText);
+      if (index !== -1) {
+        contactMethod.splice(index, 1);
+      }
+    }
+  });
+});
