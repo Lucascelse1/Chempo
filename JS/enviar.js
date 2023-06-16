0eddbc884cbff108b11441c5529605b4ea0e9156
Facundo <facundo.belsito@gmail.com>
2023-06-15 22:01:16 -0300

    prueba

--- JS/enviar.js
+++ JS/enviar.js
@@ -8,35 +8,17 @@ document.getElementById('form').addEventListener('submit', function(event) {
   const serviceID = 'default_service';
   const templateID = 'template_ooqaes4';
 
-  // Obtener todos los elementos de radio button del formulario
-  const contactMethods = document.getElementsByName('contact_method');
-  let selectedMethod = '';
-
-  // Encontrar el método seleccionado
-  for (const method of contactMethods) {
-    if (method.checked) {
-      selectedMethod = method.value;
-      break;
-    }
-  }
-
-  // Agregar el valor seleccionado al objeto de datos del formulario
-  const formData = {
-    from_name: this.from_name.value,
-    apellido_id: this.apellido_id.value,
-    email_id: this.email_id.value,
-    celular_id: this.celular_id.value,
-    contact_method: selectedMethod, // Agregar el valor del campo de radio button al objeto de datos
-    message: this.message.value
-  };
-
-  emailjs.send(serviceID, templateID, formData)
+  emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
-      alert('Mensaje enviado exitosamente!');
-      document.getElementById('form').reset(); // Reinicia el formulario
+      btn.value = 'Enviar';
+      alert('Formulario enviado correctamente!');
+      resetForm();
     }, (err) => {
-      btn.value = 'Send Email';
+      btn.value = 'Enviar';
       alert(JSON.stringify(err));
     });
 });
+
+function resetForm() {
+  document.getElementById('form').reset();
+}
