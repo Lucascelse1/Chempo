2c40b05a5bcf01d7818e4520ab2f4e2bac756cf8
Lucas Celse <lucas.j.celse@gmail.com>
2023-06-20 16:40:49 -0300

    ultimos cambios

--- JS/prueba.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn');
-const navUl = document.querySelector('.nav-ul');
-
-menuBtn.addEventListener('click', function () {
-  navUl.classList.toggle('show');
-});
