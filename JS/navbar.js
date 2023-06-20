3cf02491c3d837e660b62742e36db0f83a256803
Lucas Celse <lucas.j.celse@gmail.com>
2023-06-19 22:21:51 -0300

    ultimos cambios

--- JS/navbar.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("navbar-toggle")
-btn.addEventListener('click', toggleNavbar)
-
-
-function toggleNavbar(e) {
-    e.preventDefault()
-    let navbarList = document.getElementById("navbarList");
-    navbarList.classList.toggle("show");
-  }
